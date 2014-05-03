018a936d1ccc89c76a1374b855d936b22eb42e07
Igor Ramos Tiburcio <irtigor@csu.fullerton.edu>
2014-05-02 20:31:41 -0700

    Modified the main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 // Igor Ramos Tiburcio
 #include <iostream>
-
-int add(int x, int y)
-{
-    return x + y;
-}
+#include "add.h"
 
 int main()
 {
     using namespace std;
     cout << "Hello!" << endl;
     cout << "The sum of 10 and 4 is " << add(10, 4) << endl;
+    cout << "The sum of 11 and 4 is " << add(10, 4) << endl;
     return 0;
 }
