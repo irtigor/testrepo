864276dd0352c99c33d3e07324e5a0ec444d069a
Igor Ramos Tiburcio <irtigor@csu.fullerton.edu>
2014-05-02 20:37:09 -0700

    Small bug fixed

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main()
     using namespace std;
     cout << "Hello!" << endl;
     cout << "The sum of 10 and 4 is " << add(10, 4) << endl;
-    cout << "The sum of 11 and 4 is " << add(10, 4) << endl;
+    cout << "The sum of 11 and 4 is " << add(11, 4) << endl;
     return 0;
 }
