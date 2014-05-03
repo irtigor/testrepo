f6d593c3e6719ad14aafb48127a43abeb28c34c8
Igor Ramos Tiburcio <irtigor@csu.fullerton.edu>
2014-05-02 20:18:30 -0700

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Igor Ramos Tiburcio
+#include <iostream>
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+int main()
+{
+    using namespace std;
+    cout << "Hello!" << endl;
+    cout << "The sum of 10 and 4 is " << add(10, 4) << endl;
+    return 0;
+}
